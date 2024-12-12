8a7e4916aade8158a4456a4ba05c711eab985c9a
miguel-goyena-cv <miguel_goyena@cuatrovientos.org>
2024-12-12 08:42:16 +0100

    Ejemplos de Pipes y Ciclos del componente

--- src/app/fotoperfil/fotoperfil.component.ts
+++ src/app/fotoperfil/fotoperfil.component.ts
@@ -1,9 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, input, SimpleChanges } from '@angular/core';
 import { LoggerService } from '../servicios/logger.service';
+import {UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-fotoperfil',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './fotoperfil.component.html',
   styleUrl: './fotoperfil.component.scss'
 })
@@ -19,5 +20,15 @@ export class FotoperfilComponent {
   ocultarNombre() {
     this.logger.logDebugConAlerts("Le ha dado al boton, mi variable nombreOacultado es: "+this.nombreOcultado);
     this.nombreOcultado = !this.nombreOcultado;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Hay cambios');
+    for (const inputName in changes) {
+      const inputValues = changes[inputName];
+      console.log(`Previous ${inputName} == ${inputValues.previousValue}`);
+      console.log(`Current ${inputName} == ${inputValues.currentValue}`);
+      console.log(`Is first ${inputName} change == ${inputValues.firstChange}`);
+    }
   }
 }
